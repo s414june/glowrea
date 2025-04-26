c407f5a03cab6f78c6b783305b4a69e791dc94b2
s414june <s414june@gmail.com>
2025-04-26 20:37:19 +0800

    feat: 跳轉重設憑證

--- components/timeline/TimelineNotifications.vue
+++ components/timeline/TimelineNotifications.vue
@@ -7,13 +7,18 @@ const { filter } = defineProps<{
 }>()
 
 const route = useRoute()
+const paginator = ref()
+const stream = ref()
 const lastAccessedNotificationRoute = useLocalStorage(STORAGE_KEY_LAST_ACCESSED_NOTIFICATION_ROUTE, '')
 
 const options = { limit: 30, types: filter ? [filter] : [] }
 
 // Default limit is 20 notifications, and servers are normally caped to 30
-const paginator = useMastoClient().v1.notifications.list(options)
-const stream = useStreaming(client => client.user.notification.subscribe())
+watch(() => route.fullPath, async () => {
+  setCustomClient()
+  paginator.value = useMastoClient().v1.notifications.list(options)
+  stream.value = useStreaming(client => client.user.notification.subscribe())
+}, { immediate: true })
 
 lastAccessedNotificationRoute.value = route.path.replace(/\/notifications\/?/, '')
 
