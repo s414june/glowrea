e89aa1bbb1c04744432965b976983c1c2ed4dfad
s414june <s414june@gmail.com>
2025-04-26 20:23:31 +0800

    feat: 切換伺服器按鈕

--- pages/[[server]]/public/local.vue
+++ pages/[[server]]/public/local.vue
@@ -9,10 +9,15 @@ useHydratedHead({
 <template>
   <MainContent>
     <template #title>
-      <NuxtLink to="/public/local" timeline-title-style flex items-center gap-2 @click="$scrollToTop">
-        <div i-ri:group-2-line />
-        <span>{{ t('title.local_timeline') }}</span>
-      </NuxtLink>
+      <div class="flex justify-between grow">
+        <NuxtLink to="/public/local" timeline-title-style flex items-center gap-2 @click="$scrollToTop">
+          <div i-ri:group-2-line />
+          <span>{{ t('title.local_timeline') }}</span>
+        </NuxtLink>
+        <button btn-outline text-sm px-2 py-1 xl:hidden @click="changeSwitchServerDialogDisplay(true)">
+          {{ $t('action.switch_server') }}
+        </button>
+      </div>
     </template>
 
     <TimelinePublicLocal v-if="isHydrated" />
