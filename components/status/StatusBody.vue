57356b13a1e0ed471175eb63d3ff286204ba2fa0
s414june <s414june@gmail.com>
2025-07-16 01:04:51 +0800

    fix 收合ssr

--- components/status/StatusBody.vue
+++ components/status/StatusBody.vue
@@ -32,52 +32,59 @@ const vnode = computed(() => {
 })
 
 onMounted(() => {
-  if (statusBodyComponent.value) {
-    const originHeight = statusBodyComponent.value.scrollHeight
-    statusBodyComponent.value.setAttribute('clip', 'true')
+  watch(() => statusBodyComponent.value, async () => {
+    if (statusBodyComponent.value) {
+      const originHeight = statusBodyComponent.value.scrollHeight
+      statusBodyComponent.value.dataset.clip = 'true'
 
-    const newHeight = statusBodyComponent.value?.scrollHeight
-    if (originHeight === newHeight) {
-      statusBodyComponent.value.setAttribute('clip', 'false')
+      const newHeight = statusBodyComponent.value?.scrollHeight
+      if (originHeight - newHeight < 1) {
+        statusBodyComponent.value.dataset.clip = 'false'
+      }
+      else {
+        hasMore.value = true
+      }
     }
-    else {
-      hasMore.value = true
-    }
-  }
+  }, {
+    immediate: true,
+    flush: 'post',
+  })
 })
 </script>
 
 <template>
-  <div
-    ref="statusBodyComponent" class="status-body" whitespace-pre-wrap break-words
-    :class="{ 'with-action': withAction, 'close-more': !openMore }" relative
-  >
-    <span
-      v-if="status.content" class="content-rich line-compact flex flex-col" dir="auto"
-      :lang="('language' in status && status.language) || undefined"
+  <ClientOnly>
+    <div
+      ref="statusBodyComponent" class="status-body" whitespace-pre-wrap break-words
+      :class="{ 'with-action': withAction, 'close-more': !openMore }" relative
     >
-      <component :is="vnode" v-if="vnode" />
-    </span>
-    <div v-else />
-    <button
-      v-show="hasMore && !openMore" text-primary m="t-2" i-ri:arrow-down-double-line type="button"
-      class="action-button" @click="openMore = true"
-    />
-    <button
-      v-show="hasMore && openMore" text-primary m="t-2" i-ri:arrow-up-double-line type="button"
-      class="action-button" @click="openMore = false"
-    />
-    <template v-if="translation.visible">
-      <div my2 h-px border="b-2 base" bg-base />
-      <ContentRich
-        v-if="translation.success" class="line-compact" :content="translation.text"
-        :emojis="status.emojis"
+      <span
+        v-if="status.content" class="content-rich line-compact" dir="auto"
+        :lang="('language' in status && status.language) || undefined"
+      >
+        <component :is="vnode" v-if="vnode" />
+      </span>
+      <div v-else />
+      <button
+        v-show="hasMore && !openMore" text-primary m="t-2" i-ri:arrow-down-double-line type="button"
+        class="action-button" @click="openMore = true"
       />
-      <div v-else text-red-4>
-        Error: {{ translation.error }}
-      </div>
-    </template>
-  </div>
+      <button
+        v-show="hasMore && openMore" text-primary m="t-2" i-ri:arrow-up-double-line type="button"
+        class="action-button" @click="openMore = false"
+      />
+      <template v-if="translation.visible">
+        <div my2 h-px border="b-2 base" bg-base />
+        <ContentRich
+          v-if="translation.success" class="line-compact" :content="translation.text"
+          :emojis="status.emojis"
+        />
+        <div v-else text-red-4>
+          Error: {{ translation.error }}
+        </div>
+      </template>
+    </div>
+  </ClientOnly>
 </template>
 
 <style scoped>
@@ -85,7 +92,7 @@ onMounted(() => {
   cursor: pointer;
 }
 
-.status-body.with-action[clip=true].close-more>* {
+.status-body.with-action[data-clip="true"].close-more>* {
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 10;
